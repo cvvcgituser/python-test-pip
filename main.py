1c4ce9495f2872691b2a6d1862885a9c3fda0859
cvvcgituser <115985101+cvvcgituser@users.noreply.github.com>
2022-12-27 10:19:48 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     """Return a friendly HTTP greeting."""
-    return 'Hello World'
+    return 'Hi ACS !,Python App for test deployment in Amazon ECS'
 
 @app.errorhandler(500)
 def server_error(e):
